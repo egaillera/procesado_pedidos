4ba323b88008a6889a82d7073f6d3b8eb8b0f6d0
Enrique Garcia Illera <egaillera@gmail.com>
2024-10-03 17:01:29 +0200

    [feat] reviewed categories and formats, and some code cleaning

--- extraction_agent.py
+++ extraction_agent.py
@@ -10,7 +10,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_openai.embeddings import OpenAIEmbeddings
 
 from pydantic import BaseModel, Field
-from typing import List,Optional, Literal
+from typing import List,Optional, Literal, Set
 import pandas as pd
 
 import os
@@ -18,7 +18,7 @@ import os
 from dotenv import load_dotenv
 
 categories = ["jamon","paleta","presa","chorizo", "morcilla","salchichon","avio","panceta",
-             "caldo","crema","pate","queso cabra","queso oveja","queso mezcla oveja y cabra"
+             "caldo","crema","pate","queso cabra","queso oveja","queso mezcla oveja y cabra",
             "miel","aceitunas","other"]
 
 qualities = ["bellota iberico","cebo iberido","cebo campo iberico","bellota iberica 100%",
@@ -28,9 +28,11 @@ qualities = ["bellota iberico","cebo iberido","cebo campo iberico","bellota iber
 
 weights = ["100 gramos","250 gramos","400 gramos","500 gramos","750 gramos","1 kilogramo", "2 kilogramos"]
 
-formats = ["loncheado","virutas","tarro","embuchado","doblado","cular","herradura"]
+formats = ["loncheado","virutas","tarro","embuchado","doblado","cular","herradura","cortado","vela"]
 
 tastes = ["atun","finas hierbas","iberico","pimenton","corsevilla","sabor tradicional","picante","blanco","rojo"]
+
+path_to_catalog = "./data/CatalogoEstructurado.xlsx"
 
 
 
@@ -58,21 +60,7 @@ def read_products(excel_file):
 
     df = pd.read_excel(excel_file)
 
-    return ";".join(list(df["Descripcion"]))
-
-    '''
-    # Initialize the dictionary: one array of products per family
-    for family in df["Familia"].unique():
-        catalogue[family] = []
-
-
-    for index, row in df.iterrows():
-        catalogue[row['Familia']].append({"Articulo":row['Articulo'],
-                                          "Descripcion":row["Descripcion"]})
-
-    json_string = json.dumps(catalogue,indent=4).replace("{", "{{").replace("}", "}}")
-    return json_string,vectorstore
-    '''
+    return ";".join(list(df["DESCRIPCION"]))
 
 
 
@@ -88,8 +76,8 @@ def create_extraction_agent():
         quality: Optional[str] = Field(description="adjective that indicates quality of the product",
                                        examples=qualities)
         weight: Optional[str] = Field(description="Weight of the product", examples=weights)
-        format: Optional[str] = Field(description="Format, which can describe both the type of product cut and the packaging",
-                                      examples=formats)
+        format: Optional[Set[Literal[*formats]]] = Field(description="Format, which can describe both the type of product cut and the packaging", #type: ignore
+                                      examples=formats) 
         taste: Optional[str] = Field(description="What is the taste of the product",examples=tastes)
 
 
@@ -99,33 +87,15 @@ def create_extraction_agent():
 
     functions = [Information]
 
-    llm = ChatOpenAI(model="gpt-4o", temperature = 0)
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature = 0)
     llm_with_functions = llm.bind_functions(functions,function_call={"name":"Information"})
 
     # TO USE WITH MISTRAL
     #llm = ChatMistralAI(model="mistral-large-latest",temperature=0)
     #llm_with_functions = llm.bind_tools(functions)
 
-    '''
-    system_prompt = "Think carefully and then extract the products of the purchase order\
-                     taking into account this JSON catalogue: " + read_products("./data/Tarifas_por_familia_JyS.xls") \
-                     + "\n\n If there is ambiguity or you are not sure about the product or the amount of products, return nothing.\
-                        Examples of ambiguity: \
-                            quiero un tarro de pate (there are two kinds: PATE DE ATÚN 250 GRS and PATE IBERÍCO 250 GRS)\
-                            quiero un loncheado de jamon (there are three: LONCHEADO 100 GR JAMON CURADO C, \
-                                                          LONCHEADO 100 GR JAMON DE CEBO IBERICO and LONCHEADO 100 GR JAMON RESERVA)" '''
-    '''
-    system_prompt = "Think carefully and then extract the number of products of the purchase order. \
-                    Also for each product, you must extract: \
-                    - the amount of units  (mandatory)\
-                    - the type of product, that could be jamon, paleta, lomo, presa, chorizo, morcilla, salchichon, avio, \
-                       panceta, caldo, crema, pate, queso cabra, queso oveja, miel, aceitunas, loncheado lomo, loncheado paleta \
-                       loncheado jamon, loncheado salchichon (mandatory) \
-                    - the quality of the product, that could be iberico, bellota iberico, cebo iberico, cebo campo iberico, \
-                        bellota iberica 100%, bellota iberica seleccion, 100% iberico denominacion de origen (optional)"
-    '''
     system_prompt = "Think carefully and then extract the list of products of the purchase order, \
-        taking into account this JSON catalogue: " + read_products("./data/Tarifas_por_familia_JyS.xls")
+        taking into account this JSON catalogue: " + read_products(path_to_catalog)
 
     prompt = ChatPromptTemplate.from_messages([
         ("system", system_prompt),
@@ -152,18 +122,6 @@ def main():
         result = agent.invoke({"input":purchase})
         print(result)
 
-
-        '''
-        for item in result['products']:
-            #print(item["name"])
-            result_string += item["name"] + "\n"
-            similars = vectorstore.similarity_search_with_relevance_scores(item["name"],k=10)
-            for res,score in similars:
-                #print(f"* [SIM={score:3f}] {res.page_content}")
-                result_string += f"* [SIM={score:3f}] {res.page_content}" + "\n"
-
-        print(result_string)
-        '''
 
 if __name__ == "__main__":
     main()
